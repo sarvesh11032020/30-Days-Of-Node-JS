71fda7a738d977aaef2d345e8549abff865c3ca6
Sarvesh Suryakant Arondekar <115332581+sarvesh11030801@users.noreply.github.com>
2024-02-14 00:34:51 +0530

    Day 13 - 1

--- Day 13/day13.js
+++ Day 13/day13.js
@@ -1,24 +1,50 @@
+// const express = require('express');
+// const http = require('http');
+// const WebSocket = require('ws');
+
+// const app = express();
+// const server = http.createServer(app);
+
+// const wss = new WebSocket.Server({ server });
+
+// wss.on('connection', function connection(ws) {
+//     ws.on('message', function incoming(message) {
+//       // Echo the received message back to the client
+//       ws.send(message);
+//     });
+//   });
+
+// app.get('/websocket', function(req, res) {
+// res.sendFile(__dirname + '/websocket.html');
+// });
+
+// const PORT = process.env.PORT || 2000;
+// server.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
+
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
+const webSocket = require('ws');
 
 const app = express();
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({ server });
+const wss = new webSocket.Server({ server });
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-      // Echo the received message back to the client
-      ws.send(message);
-    });
-  });
+function setWebSocket(server){
+    wss.on('connection', (ws) => {
+        ws.on('message', (message) => {
+            ws.send(message);
+        })
+    })
+}
 
-app.get('/websocket', function(req, res) {
-res.sendFile(__dirname + '/websocket.html');
+app.get('/websocket', (req, res) => {
+    res.sendFile(path.join(__dirname + '/websocket.html'));
 });
 
-const PORT = process.env.PORT || 2000;
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+setWebSocket(server);
+server.listen(2000, () => {
+    console.log(`Server is listening`);
+})
